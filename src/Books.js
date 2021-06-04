28debe9b67b9dc464f20e0e8ec9b6d66b3e41bfb
Lubov May <liubov.mayorova@gmail.com>
2021-06-03 17:26:36 -0700

    Updated Books.js - imported new component

--- src/Books.js
+++ src/Books.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
-import CurrentlyReadingShelf from './CurrentlyReadigShelf'
-import WantToReadShelf from './WantToReadShelf'
-import Read from './Read'
+
+import Shelves from './Shelves'
 import { Link } from 'react-router-dom'
 
 class Books extends Component {
-    state = {
-        showSearchPage: false,
-    }
 
     render() {
         return (
@@ -15,15 +11,12 @@ class Books extends Component {
                 <div className="list-books-title">
                     <h1>MyReads</h1>
                 </div>
-                <CurrentlyReadingShelf books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} />
-                <WantToReadShelf books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} />
-                <Read books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} />
-
+                <Shelves books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} />
 
                 <Link to='/search'
                     className="open-search"
                 >
-                    <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+                    <button>Add a book</button>
                 </Link>
 
             </div>
