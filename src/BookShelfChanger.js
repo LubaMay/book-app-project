98f09b0130b9882853d7a06d95d5508843f3f177
Lubov May <liubov.mayorova@gmail.com>
2021-06-03 17:27:03 -0700

    Updated BookShelfChanger.js - added value to select

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -2,22 +2,21 @@ import React, { Component } from 'react'
 
 class BookShelfChanger extends Component {
 
-
     handleChange = (e) => {
         console.log('changing', this.props.book.id, this.props.book.title)
+
         if (this.props.onUpdateShelf) {
             this.props.onUpdateShelf(this.props.book, e.target.value)
         }
-    }
 
+    }
 
     render() {
         return (
 
             <div className="book-shelf-changer">
-
-                <select onChange={this.handleChange} >
-                    <option value="move" >Move to...</option>
+                <select onChange={this.handleChange} value={typeof (this.props.book.shelf) !== "undefined" ? this.props.book.shelf : 'none'}>
+                    <option value="move">Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
